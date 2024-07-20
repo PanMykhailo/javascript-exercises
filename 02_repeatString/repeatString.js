507172e6691b0c77930b0447125a98127e741435
Mike <mykhailoserhiienko674@gmail.com>
2024-07-20 16:46:33 +0300

    complete 2 tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+function repeatString(string, number) {
+    let returnString = '';
+    if (number < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < number; i++) {
+                returnString += string;
+                console.log(returnString);
+            }
+            return returnString;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
