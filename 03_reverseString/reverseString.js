8d03f90e7938f7a9de4566990f4edbed422a40b5
Mike <mykhailoserhiienko674@gmail.com>
2024-07-21 09:20:29 +0300

    complete reverseString task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function(string) {
-
-};
+function reverseString(string) {
+    let result = string.split('').reverse().join('');
+    console.log(result)
+    return result
+}
 
 // Do not edit below this line
 module.exports = reverseString;
