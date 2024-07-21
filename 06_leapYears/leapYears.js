ec41c08e5edb2a2ef29ff7577494f6c7df41ed21
Mike <mykhailoserhiienko674@gmail.com>
2024-07-21 18:40:17 +0300

    complete leapYears task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isCentury = year % 100 === 0;
+    if (year % 4 === 0 && !isCentury || year % 400 === 0) {
+        return true;
+    } else if (year % 4 > 0 || year % 400 > 0 || isCentury){
+        return false;
+    }
 };
 
 // Do not edit below this line
