17400bfd88c7e8c83593aaabb77a2b1c1da0a454
Mike <mykhailoserhiienko674@gmail.com>
2024-07-21 18:15:50 +0300

    complete sumAll task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    if (num1 > num2) [num1, num2] = [num2, num1];
+    
+    let result = 0;
+    for (let i = num1; i <= num2; i++) {
+        result += i;
+        console.log(result);
+    }
+    return result;
 };
 
 // Do not edit below this line
