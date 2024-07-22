c86be322c4747b48e2ab5e7898d0f57bbd02f3f5
Mike <mykhailoserhiienko674@gmail.com>
2024-07-22 11:16:44 +0300

    complete tempConversion task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  const tempInCelcius = parseFloat(((value - 32) * (5 / 9)).toFixed(1));
+  return tempInCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  const tempInFahrenheit = parseFloat((value * (9 / 5) + 32).toFixed(1));
+  return tempInFahrenheit;
 };
 
 // Do not edit below this line
